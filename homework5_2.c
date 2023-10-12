a995480949c9edd8458e7d6ea9bcfc9453c08c64
KuprikSar <i@avp1991.ru>
2023-10-12 23:58:19 +0300

    task52isdone

---
+++ homework5_2.c
@@ -0,0 +1,23 @@
+// составить функцию которая определяет максимальный общий делитель у двух натуральных чисел
+#include <stdio.h>
+
+int NOD (int a, int b) 
+{
+    if (b ==0)
+    {
+        return a;
+    }
+    return NOD(b, a % b);  
+}
+
+int main () {
+
+    int num1, num2;
+    printf("Введите два числа: ");
+    scanf("%d%d", &num1, &num2);
+
+    int result = NOD(num1, num2);
+
+    printf("Наибольший общий делитель: %d\n", result);
+    return 0;
+}
