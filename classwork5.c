9d4c35cb0012b7d2cb91c0f236f6f37ab871b4fc
KuprikSar <i@avp1991.ru>
2023-10-12 18:18:16 +0300

    classfirst2

--- classwork5.c
+++ classwork5.c
@@ -1,14 +1,35 @@
+#include <inttypes.h>
 #include <stdio.h>
+#include <ncurses.h>
+#include <locale.h>
 
-int main (int argc, char **argv) { // буферный ввод
-
-    char c;
-    int status = puts("Enter the symbol, '.' is exit:");
-    do
+int main(int argc, char **argv)
+{
+    char Choice;
+    setlocale(LC_ALL, "Rus");
+    while(1)
     {
-        c = getchar();
-        status = putchar(c);
-    } 
-       while (c != '.');
-       return 0;
+        printf("1. Вычисление корней квадратного уравнения\n");
+        printf("0. Выход\n");
+        printf("Для выход нажмите Q\n");
+NO_PRINT:
+        Choice = getch();
+        switch(Choice)
+        {
+            case '1':
+                printf("SquareEquation()\n");
+            break;
+            case '0':
+            case 'q':
+            case 'Q':
+                return 0;
+            break;
+            case 0xa://'\n':
+                goto NO_PRINT;
+            default:
+            printf("Непонятныйn выбор %x\n", Choice);
+            break;
+        }           
+    }
+    return 0;
 }
